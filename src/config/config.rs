4f07e5b42ea763f7f3f6dd32efc0685dfdfd5ed7
KisaragiEffective <kisaragi.effective@gmail.com>
2022-03-21 21:15:49 +0900

    fix: add deriving traits

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,12 +1,12 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Config {
     pub(crate) database_authorization: DatabaseAuthorizationInfo,
     pub(crate) ports: ServicePorts,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct DatabaseAuthorizationInfo {
     pub(crate) host: String,
     pub(crate) port: Port,
@@ -14,11 +14,11 @@ pub struct DatabaseAuthorizationInfo {
     pub(crate) password: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct ServicePorts {
     pub(crate) https: Port,
     pub(crate) http: Port,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Eq, PartialEq, Debug)]
 pub struct Port(pub(crate) u16);
