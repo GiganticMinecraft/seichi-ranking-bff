cb1c8e552dbc90c5991c660f46948ca692c8c397
KisaragiEffective <kisaragi.effective@gmail.com>
2022-03-21 15:31:36 +0900

    fix: missing crate and method

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
+mod handler;
+
 extern crate core;
 
 use std::fs::File;
 use std::io::BufReader;
-use std::sync::Arc;
-use actix_web::{App, guard, HttpResponse, HttpServer, trace, web};
+use actix_web::{App, HttpResponse, HttpServer, web};
 use actix_web::web::JsonConfig;
-use log::{error, info};
+use anyhow::Context;
+use log::{error, info, trace};
 use once_cell::sync::OnceCell;
 use rustls::{Certificate, PrivateKey, ServerConfig};
 use rustls_pemfile::{certs, pkcs8_private_keys};
 
 static RUNNING_CONFIG: OnceCell<Config> = OnceCell::new();
+
+fn setup_logger() -> Result<(), fern::InitError> {
+    use fern::colors::*;
+    let mut colors = ColoredLevelConfig::new();
+
+    fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                colors.color(record.level()),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Trace)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("output.log")?)
+        .apply()?;
+    Ok(())
+}
 
 fn load_ssl_keys() -> (Vec<Certificate>, PrivateKey) {
     trace!("loading cert.pem");
