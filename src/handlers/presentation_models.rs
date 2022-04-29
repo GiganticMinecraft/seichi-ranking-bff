251ba9ada61dba3a07c106af9c9e9427a1bc22fe
Ryosuke <korygm33@gmail.com>
2022-04-29 17:38:29 +0100

    refactor: chronoのserde::Serialize<DateTime>実装を使う

--- src/handlers/presentation_models.rs
+++ src/handlers/presentation_models.rs
@@ -1,28 +1,16 @@
 use crate::models::{AggregatedPlayerAttribution, RankedAttributionRecord};
 use chrono::{DateTime, Utc};
-use serde::ser::SerializeStruct;
-use serde::{Serialize, Serializer};
+use serde::Serialize;
 use uuid::Uuid;
 
+#[derive(Serialize)]
 pub(crate) struct Player {
     pub(crate) uuid: Uuid,
     pub(crate) name: String,
+    // OpenAPI Specificationでの `format: date-time` によりRFC3339形式が要求されているが、
+    // chrono 0.4.19 の `impl<Tz: TimeZone> serde::Serialize for DateTime<Tz>` は
+    // RFC3339でシリアライズするようになっている。
     pub(crate) last_quit: DateTime<Utc>,
-}
-
-impl Serialize for Player {
-    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut s = serializer.serialize_struct("Player", 3)?;
-        s.serialize_field("uuid", &self.uuid)?;
-        s.serialize_field("name", &self.name)?;
-
-        // OpenAPI Specificationでのdate-timeがRFC3339形式を要求している
-        s.serialize_field("last_quit", &self.last_quit.to_rfc3339())?;
-        s.end()
-    }
 }
 
 #[derive(Serialize)]
